519bc057f41a2ca5281d41be7395b4901c71e337
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-29 06:11:22 +0000

    💄 404 page gets revamped with personal branding

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,8 @@ export default function ErrorPage() {
     return (
         <div className="container">
             <h1 className="rainbow sans">404.</h1>
-            <p className="sans" style={{ marginTop: 0, fontSize: '30px' }}><span style={{ fontWeight: 600 }}>Looks like we couldn't find that link.</span> I'll be redirecting you to my homepage :).</p>
+            <p className="sans" style={{ marginTop: 0, fontSize: '20px' }}><span style={{ fontWeight: 600, fontSize: '30px' }}>Looks like we couldn't find that link.</span><br/>Ask the person who gave it to you to double check. :)</p>
+            <p className="sans" style={{ marginTop: '4ch', fontSize: '20px' }}>This is <code>srtk.me</code>, a link shortener made with Next.js.<br /><span style={{ fontWeight: 600 }}>Bootstrap your own <a href="https://srtk.me/gh/srtk.me-v2">here</a>!</span></p>
         </div>
     )
 }

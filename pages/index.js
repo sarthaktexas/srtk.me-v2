e44cd0fc0c33ddb5e3c412c8d90bc318cbee37ac
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-11-29 02:55:29 +0000

    🔥 clean up some code

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router'
 //   )
 // }
 
-export default function RedirectPage({ ctx }) {
+export default function RedirectPage() {
   const router = useRouter()
   if (typeof window !== 'undefined') {
     router.push('https://sarthakmohanty.me');
